e2be8d55633225a777b4a8a42fbe94b38533adc1
unknown <25393@ma-web.nl>
2020-10-30 15:53:31 +0100

    Ditbenik.py reworked

---
+++ Vragenlijst test.py
@@ -0,0 +1,85 @@
+print ("Hello You!")
+print ("Ik ben Gael Griffith")
+print ("wie ben jij?")
+print ("typ je naam in: ")
+
+inputnaam = input()
+
+print ("Hallo " + inputnaam) 
+print ("Welke kledingmerk vindt ik het leukst?")
+print ("A. Stuzzy")
+
+print ("B. Smib")
+
+print ("C. Daily paper")
+
+antwoord = input("typ A, B of C: ")
+ 
+if antwoord == "A" or antwoord =="a":
+
+     print ("nee dat klopt niet ik hou er wel van")
+
+if antwoord == "B" or antwoord =="b":
+
+     print (" jouw antwoord was: " + antwoord + ", " + " dit is goed")
+
+if antwoord == "C" or antwoord =="c":
+
+     print ("nee hou er wel van")
+
+else:
+
+     print (" je kunt alleen met A, B of C antwoorden")
+
+print ("Hou oud ben ik denk jij?")
+print ("A. 18")
+
+print ("B. 16")
+
+print ("C. 17")
+
+antwoord = input("typ A, B of C: ")
+ 
+if antwoord == "A" or antwoord =="a":
+
+     print ("nee dat klopt niet")
+
+if antwoord == "B" or antwoord =="b":
+
+     print ("nee dat klopt niet")
+
+if antwoord == "C" or antwoord =="c":
+
+     print (" jouw antwoord was: " + antwoord + ", " + " dit is goed")
+
+else:
+
+     print (" je kunt alleen met A, B of C antwoorden")
+
+print ("Waar woon ik?")
+print ("A. Utrecht")
+
+print ("B. Amsterdam")
+
+print ("C. Rotterdam")
+
+antwoord = input("typ A, B of C: ")
+ 
+if antwoord == "A" or antwoord =="a":
+
+     print ("nee dat klopt niet")
+
+if antwoord == "B" or antwoord =="b":
+
+     print (" jouw antwoord was: " + antwoord + ", " + " dit is goed")
+
+if antwoord == "C" or antwoord =="c":
+
+     print ("nee dat klopt niet")
+
+else:
+
+     print (" je kunt alleen met A, B of C antwoorden")
+
+
+
